1861815af0b975254db31bf4e553603d474cd788
Tabish Shaikh <bonjour.tabish@gmail.com>
2024-10-31 17:02:07 +0530

    feat: use oz as import

--- src/CrossOwnable.sol
+++ src/CrossOwnable.sol
@@ -1,36 +1,12 @@
 // SPDX-License-Identifier: MIT
-// OpenZeppelin Contracts (last updated v5.0.0) (access/Ownable.sol)
 
 pragma solidity ^0.8.20;
 
-import {Context} from "./utils/Context.sol";
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
 
-/**
- * @dev Contract module which provides a basic access control mechanism, where
- * there is an account (an owner) that can be granted exclusive access to
- * specific functions.
- *
- * The initial owner is set to the address provided by the deployer. This can
- * later be changed with {transferOwnership}.
- *
- * This module is used through inheritance. It will make available the modifier
- * `onlyOwner`, which can be applied to your functions to restrict their use to
- * the owner.
- */
-abstract contract Ownable is Context {
-    address private _owner;
+abstract contract CrossOwnable is Ownable {
     address public previousContract;
     bool public isSuperOwner;
-
-    /**
-     * @dev The caller account is not authorized to perform an operation.
-     */
-    error OwnableUnauthorizedAccount(address account);
-
-    /**
-     * @dev The owner is not a valid owner account. (eg. `address(0)`).
-     */
-    error OwnableInvalidOwner(address owner);
 
     /**
      * @dev The caller is not previousContract.
@@ -46,26 +22,12 @@ abstract contract Ownable is Context {
 
     event PreviousContractUpdated(address indexed oldContract, address indexed newContract);
 
-    event OwnershipTransferred(address indexed previousOwner, address indexed newOwner);
-
     /**
      * @dev Initializes the contract setting the address provided by the deployer as the initial owner.
      */
-    constructor(address initialOwner, address _previousContract, bool _isSuperOwner) {
-        if (initialOwner == address(0)) {
-            revert OwnableInvalidOwner(address(0));
-        }
-        _transferOwnership(initialOwner);
+    constructor(address initialOwner, address _previousContract, bool _isSuperOwner) Ownable(initialOwner) {
         _setPreviousContract(_previousContract);
         _setIsSuperOwner(_isSuperOwner);
-    }
-
-    /**
-     * @dev Throws if called by any account other than the owner.
-     */
-    modifier onlyOwner() {
-        _checkOwner();
-        _;
     }
 
     /**
@@ -82,7 +44,7 @@ abstract contract Ownable is Context {
      * @dev Throws if called by any account other than previousContract.
      */
     modifier onlySuperOwner() {
-        if(!isSuperOwner && previousContract != address(0)) {
+        if (!isSuperOwner && previousContract != address(0)) {
             revert OwnableInvalidSuperOwner();
         }
         _;
@@ -127,29 +89,13 @@ abstract contract Ownable is Context {
     }
 
     /**
-     * @dev Returns the address of the current owner.
-     */
-    function owner() public view virtual returns (address) {
-        return _owner;
-    }
-
-    /**
-     * @dev Throws if the sender is not the owner.
-     */
-    function _checkOwner() internal view virtual {
-        if (owner() != _msgSender()) {
-            revert OwnableUnauthorizedAccount(_msgSender());
-        }
-    }
-
-    /**
      * @dev Leaves the contract without owner. It will not be possible to call
      * `onlyOwner` functions. Can only be called by the current owner.
      *
      * NOTE: Renouncing ownership will leave the contract without an owner,
      * thereby disabling any functionality that is only available to the owner.
      */
-    function renounceOwnership() public virtual onlyOwner onlySuperOwner {
+    function renounceOwnership() public override onlyOwner onlySuperOwner {
         _transferOwnership(address(0));
     }
 
@@ -168,7 +114,7 @@ abstract contract Ownable is Context {
      * @dev Transfers ownership of the contract to a new account (`newOwner`).
      * Can only be called by the current owner.
      */
-    function transferOwnership(address newOwner) public virtual onlyOwner onlySuperOwner {
+    function transferOwnership(address newOwner) public override onlyOwner onlySuperOwner {
         if (newOwner == address(0)) {
             revert OwnableInvalidOwner(address(0));
         }
@@ -194,10 +140,8 @@ abstract contract Ownable is Context {
      * @dev Transfers ownership of the contract to a new account (`newOwner`).
      * Internal function without access restriction.
      */
-    function _transferOwnership(address newOwner) internal virtual {
-        address oldOwner = _owner;
-        _owner = newOwner;
-        emit OwnershipTransferred(oldOwner, newOwner);
+    function _transferOwnership(address newOwner) internal override {
+        super._transferOwnership(newOwner);
 
         // Pass cross-chain message
         _sendCrossChainMessage(newOwner);
